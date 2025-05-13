18f7ddee29502e5dfa81ca984ed7e2226323142f
Se@n <deity@Alienware-ROGc.local>
2025-05-13 17:31:43 +0800

    init commit. Implement all TODOs manually.

---
+++ android/app/src/main/kotlin/org/seanw/flutter_destini/MainActivity.kt
@@ -0,0 +1,5 @@
+package org.seanw.flutter_destini
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:destini_challenge_starting/story_brain.dart';
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(Destini());
+
+class Destini extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+ home: StoryPage(),
+    );
+  }
+}
+
+StoryBrain sb = StoryBrain();
+
+class StoryPage extends StatefulWidget {
+  _StoryPageState createState() => _StoryPageState();
+}
+
+class _StoryPageState extends State<StoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(image: 
+          AssetImage('images/background.png')),
+        ),
+ padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+ constraints: BoxConstraints.expand(),
+ child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+ children: <Widget>[
+              Expanded(
+                flex: 12,
+ child: Center(
+                  child: Text(
+                    sb.getStory().storyTitle,
+ style: TextStyle(
+                      fontSize: 25.0,
+                    ),
+                  ),
+                ),
+              ),
+ Expanded(
+                flex: 2,
+ child: TextButton(
+                  onPressed: () {
+                    //Choice 1 made by user.
+                    setState(() {
+                      sb.nextStory(1);
+                    });
+                  },
+ style: TextButton.styleFrom(
+                    backgroundColor: Colors.red,
+                  ),
+ child: Text(
+
+                    sb.getChoice1(),
+ style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+ SizedBox(
+                height: 20.0,
+              ),
+ Expanded(
+                flex: 2,
+                
+ child: Visibility(
+                  visible: sb.buttonShouldBeVisible(),
+ child: TextButton(
+                    onPressed: () {
+                      //Choice 2 made by user.
+                      setState(() {
+                        sb.nextStory(2);
+                      });
+                    },
+ style: TextButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                    ),
+ child: Text(
+
+                      sb.getChoice2(),
+ style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
